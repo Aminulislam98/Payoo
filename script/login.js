3245c190c40efd6015ef8a440ebd56fbe54256ac
Aminul islam <aminurrohman98@gmail.com>
2026-02-17 23:00:31 +0000

    payoo login page complete and made it interactive through using JavaScript

---
+++ script/login.js
@@ -0,0 +1,18 @@
+let loginButton = document.getElementById("login-btn");
+loginButton.addEventListener("click", () => {
+  let phoneNumberInput = document.getElementById("phoneNumber");
+  let pinDigitInput = document.getElementById("pinDigit");
+  let phoneNumber = phoneNumberInput.value;
+  let pinDigit = pinDigitInput.value;
+
+  if (phoneNumber.length === 11 && pinDigit.length === 4) {
+    alert("Logged in Successfully");
+    window.location.assign("/homePage.html");
+    phoneNumberInput.value = "";
+    pinDigitInput.value = "";
+    return;
+  } else {
+    alert("Login Failed");
+    return;
+  }
+});
